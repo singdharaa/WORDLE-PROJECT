d194f233ccd49667b0d0b2f593a87f27b283f25e
Dharaa Singh <230111019@hbtu.ac.in>
2026-03-12 22:21:22 +0530

    feat: Add constants

---
+++ wordle-project.py
@@ -0,0 +1,20 @@
+import random
+
+# Constants
+WORDS = [
+    "apple", "brain", "chair", "dance", "eagle",
+    "flame", "ghost", "house", "image", "juice",
+    "knife", "lemon", "money", "night", "ocean",
+    "piano", "queen", "river", "stone", "train",
+    "under", "vivid", "world", "xenon", "youth",
+    "album", "beach", "cloud", "dream", "earth",
+    "float", "grape", "heart", "input", "jazzy",
+    "kiosk", "liver", "maple", "novel", "olive",
+    "pride", "quest", "royal", "solar", "tower",
+    "ultra", "value", "whale", "xylem", "zebra",
+    "angel", "blank", "crane", "drift", "equal",
+    "fresh", "grill", "honey", "index", "jewel",
+]
+
+MAX_GUESSES = 6
+WORD_LENGTH = 5 
