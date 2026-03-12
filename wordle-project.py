69fa57911e897b6df72ac50482fd1b11537bd1ad
Dharaa Singh <230111019@hbtu.ac.in>
2026-03-12 22:35:33 +0530

    feat: Declare Stats Distribution, Alphabet Set and Welcome Screen

--- wordle-project.py
+++ wordle-project.py
@@ -16,5 +16,29 @@ WORDS = [
     "fresh", "grill", "honey", "index", "jewel",
 ]
 
+ALL_LETTERS = set("abcdefghijklmnopqrstuvwxyz")
+
 MAX_GUESSES = 6
 WORD_LENGTH = 5 
+
+
+stats_dict = {
+    "played": 0, 
+    "won": 0, 
+    "streak": 0,
+    "max-streak": 0,
+    "guess-distribution": {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+    }
+}
+
+# Game Logic
+print("="*50)
+print("Welcome to Worlde!")
+print("="*50)
+
